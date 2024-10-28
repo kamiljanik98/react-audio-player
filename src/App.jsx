4411b74b705e9201feecdceb076b11e134c83c42
yaneczq <yaneczq1@gmail.com>
2024-10-28 21:00:26 +0100

    Update styles and video scaling

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import VideoBackground from "./components/VideoBackground/VideoBackground";
 
 const App = () => {
   return (
-    <VideoBackground src="https://videos.pexels.com/video-files/9239758/9239758-uhd_2560_1440_25fps.mp4">
+    <VideoBackground src="https://videos.pexels.com/video-files/2836314/2836314-uhd_2560_1440_24fps.mp4">
       <AudioPlayer />
     </VideoBackground>
   );
